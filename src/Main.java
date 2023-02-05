d9d40dfdabb69b72db53ef2241df2c77d7f199ee
Ian Henderson <ian.henderson1215@gmail.com>
2023-02-04 23:19:37 -0800

    completed sum project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+
+import javax.management.RuntimeErrorException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner userInput = new Scanner(System.in);
+
+            System.out.println("Welcome to the Sum Program!");
+
+
+                try {
+                    System.out.println("Please input the FIRST number you would like to add: ");
+                    double a = userInput.nextDouble();
+                    System.out.println("Next, please input the SECOND number you would like to add: ");
+                    double b = userInput.nextDouble();
+                    double results = a + b;
+                    System.out.println("The sum of " + a + " + " + b + " is: " + results);
+
+                } catch (Exception e) {
+                    System.out.println("You input a letter or symbol, please restart and type ONLY a number.");
+
+
+                }
     }
 }
